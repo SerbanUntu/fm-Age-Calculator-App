0cc12d9588123e83f430b8b64e2212bd81336c1b
Șerban Untu <133024956+SerbanUntu@users.noreply.github.com>
2024-03-23 19:03:29 +0200

    feat: css colors and fonts setup

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <div class="font-normal">Normal Text</div>
+  <div class="font-bold">Bold Text</div>
+  <div class="font-extrabold">Extrabold Text</div>
+  <div class="font-normal italic">Normal Italic Text</div>
+  <div class="font-bold italic">Bold Italic Text</div>
+  <div class="font-extrabold italic">Extrabold Italic Text</div>
 </template>
