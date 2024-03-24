2c62044c4840f57a90dbea34645d4d60639634a3
Șerban Untu <133024956+SerbanUntu@users.noreply.github.com>
2024-03-24 18:55:41 +0200

    feat: form validation

--- app.vue
+++ app.vue
@@ -1,7 +1,46 @@
 <script setup>
-const inputDay = ref(null)
-const inputMonth = ref(null)
-const inputYear = ref(null)
+import { useVuelidate } from '@vuelidate/core';
+import { required, requiredIf, minLength, minValue, maxValue, helpers } from '@vuelidate/validators';
+
+const formData = reactive({
+  day: '',
+  month: '',
+  year: '',
+});
+
+const validDate = () => {
+  const date = new Date(`${formData.year}-${formData.month}-${formData.day}`);
+  let tempDay = date.getDate().toString()
+  if(tempDay.length === 1) tempDay = `0${tempDay}`
+  return tempDay === formData.day || formData.year === '' || formData.month === '' || formData.day === '';
+}
+
+const rules = computed(() => {
+  return {
+    day: {
+      required: helpers.withMessage('This field is required', required),
+      minLength: helpers.withMessage('Please add leading 0s', minLength(2)),
+      minValue: helpers.withMessage('Must be a valid day', minValue('01')),
+      maxValue: helpers.withMessage('Must be a valid day', maxValue('31')),
+      validDate: helpers.withMessage('Must be a valid date', validDate)
+    },
+    month: {
+      required: helpers.withMessage('This field is required', required),
+      minLength: helpers.withMessage('Please add leading 0s', minLength(2)),
+      minValue: helpers.withMessage('Must be a valid month', minValue('01')),
+      maxValue: helpers.withMessage('Must be a valid month', maxValue('12')),
+      validDate
+    },
+    year: {
+      required: helpers.withMessage('This field is required', required),
+      minLength: helpers.withMessage('Must be a valid year', minLength(4)),
+      maxValue: helpers.withMessage('Must be in the past', maxValue(new Date().getFullYear().toString())),
+      validDate
+    },
+  };
+});
+
+const v$ = useVuelidate(rules, formData);
 
 const years = ref(null)
 const months = ref(null)
@@ -9,13 +48,13 @@ const days = ref(null)
 
 function calculate() {
   let now = new Date()
-  let then = new Date(`${inputYear.value}-${inputMonth.value}-${inputDay.value}`)
+  let then = new Date(`${formData.year}-${formData.month}-${formData.day}`)
   years.value = now.getUTCFullYear() - then.getUTCFullYear()
   months.value = now.getMonth() - then.getMonth()
-  if(then.getDate() > now.getDate()) {
+  if (then.getDate() > now.getDate()) {
     months.value -= 1
   }
-  if(months.value < 0) {
+  if (months.value < 0) {
     months.value += 12
     years.value -= 1
   }
@@ -32,30 +71,46 @@ function calculate() {
 <template>
   <div id="app-container"
     class="text-base font-normal font-poppins bg-[--off-white] w-screen h-screen flex items-center justify-center">
-    <div class="bg-white p-10 rounded-xl rounded-br-[25 %] flex flex-col gap-1 max-w-[900px] w-[75%]">
+    <div class="bg-white p-10 rounded-xl rounded-br-[25%] flex flex-col gap-1 max-w-[900px] w-[75%]">
       <div class="flex flex-row gap-5">
         <div class="flex flex-col">
-          <label class="text-[0.5rem] text-[--smokey-grey] font-bold tracking-[0.15rem]">DAY</label>
+          <label class="text-[0.5rem] font-bold tracking-[0.15rem]" 
+          :class="{'text-[--light-red]': v$.day.$error, 'text-[--smokey-grey]': !v$.day.$error}">DAY</label>
           <input
             class="w-20 py-1 px-2.5 border border-[--light-grey] rounded-[5px] text-[--off-black] font-bold focus:border-[--purple] focus:outline-none"
-            placeholder="DD" maxlength="2" required pattern="\d{2}" v-model="inputDay" />
+            placeholder="DD" maxlength="2" v-model="formData.day"
+            :class="{'border-[--light-red]': v$.day.$error}" />
+          <p class="text-[14px] italic text-[--light-red]" v-if="v$.day.$error">{{ v$.day.$errors[0].$message }}</p>
         </div>
         <div class="flex flex-col">
-          <label class="text-[0.5rem] text-[--smokey-grey] font-bold tracking-[0.15rem]">MONTH</label>
+          <label class="text-[0.5rem] font-bold tracking-[0.15rem]"
+          :class="{'text-[--light-red]': v$.month.$error, 'text-[--smokey-grey]': !v$.month.$error}">MONTH</label>
           <input
             class="w-20 py-1 px-2.5 border border-[--light-grey] rounded-[5px] text-[--off-black] font-bold focus:border-[--purple] focus:outline-none"
-            placeholder="MM" maxlength="2" required pattern="\d{2}" v-model="inputMonth" />
+            placeholder="MM" maxlength="2" v-model="formData.month"
+            :class="{'border-[--light-red]': v$.month.$error}" />
+          <p class="text-[14px] italic text-[--light-red]" v-if="v$.month.$error">{{ v$.month.$errors[0].$message }}</p>
         </div>
         <div class="flex flex-col">
-          <label class="text-[0.5rem] text-[--smokey-grey] font-bold tracking-[0.15rem]">YEAR</label>
+          <label class="text-[0.5rem] font-bold tracking-[0.15rem]"
+          :class="{'text-[--light-red]': v$.year.$error, 'text-[--smokey-grey]': !v$.year.$error}">YEAR</label>
           <input
             class="w-20 py-1 px-2.5 border border-[--light-grey] rounded-[5px] text-[--off-black] font-bold focus:border-[--purple] focus:outline-none"
-            placeholder="YYYY" maxlength="4" required pattern="\d{4}" v-model="inputYear" />
+            placeholder="YYYY" maxlength="4" v-model="formData.year"
+            :class="{'border-[--light-red]': v$.year.$error}" />
+          <p class="text-[14px] italic text-[--light-red]" v-if="v$.year.$error">{{ v$.year.$errors[0].$message }}</p>
         </div>
       </div>
       <div class="flex flex-row relative items-center">
         <div class="h-[1px] w-full bg-[--light-grey]" />
-        <button :class="{'p-3 bg-[--off-black] rounded-full w-fit ml-auto' : true, 'bg-[--purple]' : years !== null}" @click="calculate()">
+        <button :class="{ 'p-3 bg-[--off-black] rounded-full w-fit ml-auto': true, 'bg-[--purple]': years !== null }"
+          @click="() => {
+              v$.$validate();
+              if(!v$.$invalid) {
+                calculate();
+                v$.$reset();
+              }
+            }">
           <img src="/assets/images/icon-arrow.svg" />
         </button>
       </div>
